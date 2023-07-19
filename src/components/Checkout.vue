a7027ab6ff682c801acc08ab040e6eb853e1996f
bendeca <decapitb@gmail.com>
2023-07-19 06:48:36 +0000

    Update Checkout.vue

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -3,41 +3,49 @@
 import { computed } from "@vue/runtime-core";
 // eslint-disable-next-line  no-unused-vars
 import { RouterLink, RouterView } from "vue-router";
+import moment from "moment";
 
 export default {
   data() {
     return {
       showThankYou: false,
+      memberStatus: "",
       shops: {
         bike: {
           title: "Velo Werkstatt",
           slug: "velo",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         },
         wood: {
           title: "Holz Werkstatt",
           slug: "holz",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         },
         pottery: {
           title: "Ton Werkstatt",
           slug: "ton",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         },
         digital: {
           title: "Digital Werkstatt",
           slug: "digital",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         },
         textile: {
           title: "Textil Atelier",
           slug: "textil",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         },
         screenPrint: {
           title: "Siebdruck Atelier",
           slug: "siebdruck",
-          isActive: false
+          isActive: false,
+          isAvilableAtLocation: false
         }
       }
     };
@@ -45,22 +53,45 @@ export default {
   props: {
     userId: String
   },
-  methods: {
-    checkout() {
-      fetch("https://macherblatt2022-c78d.restdb.io/rest/macherblatt", {
-        method: "POST",
+  mounted() {
+    fetch(
+      "https://macherblatt2022-c78d.restdb.io/rest/standort/64b7784817bd7965000187f7",
+      {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
           "x-apikey": "62115f2e34fd62156585883b"
-        },
-        body: JSON.stringify({
-          webling_user_id: this.$route.query.userId,
-          workshops: this.getUsedWorkshops()
-        })
-      }).then(data => {
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        for (let i = 0; i < data.availableWorkshops.length; i++) {
+          this.shops[data.availableWorkshops[i]].isAvilableAtLocation = true;
+        }
+      });
+  },
+  methods: {
+    checkout() {
+      fetch(
+        "https://macherblatt2022-c78d.restdb.io/rest/macherblatt/" +
+          this.$route.query.userId,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "x-apikey": "62115f2e34fd62156585883b"
+          },
+          body: JSON.stringify({
+            shopsUsed: this.getUsedWorkshops(),
+            checkoutTime: moment()
+          })
+        }
+      ).then(data => {
         console.log(data);
         this.showThankYou = true;
-        setTimeout(this.$router.push({ path: "/" }), 2500);
+        setTimeout(this.goHome(), 2500);
       });
     },
     getUsedWorkshops() {
@@ -71,6 +102,9 @@ export default {
         }
       }
       return used;
+    },
+    goHome() {
+      this.$router.push({ name: "home" });
     }
   },
   computed: {
@@ -83,6 +117,9 @@ export default {
         this.shops.textile.isActive ||
         this.shops.screenPrint.isActive
       );
+    },
+    memberStatusSelected() {
+      return true;
     }
   }
 };
@@ -93,19 +130,37 @@ export default {
     <h5 class="shops-title">Welche Werkstatt hast du heute genutzt?</h5>
     <div class="shop-panels">
       <div v-for="(shop, key, index) in shops" :key="index">
-        <div class="shop-panel" :class="{ active: false }">
-          <label
-            class="shop-panel-container"
-            :class="{ checked: shops[key].isActive }"
-          >
-            <img
-              class="shop-panel-bg"
-              :src="'./assets/' + shops[key].slug + '.jpeg'"
-            />
-            <input type="checkbox" :id="key" v-model="shops[key].isActive" />
-          </label>
+        <div v-if="shops[key].isAvilableAtLocation">
+          <div class="shop-panel" :class="{ active: false }">
+            <label
+              class="shop-panel-container"
+              :class="{ checked: shops[key].isActive }"
+            >
+              <img
+                class="shop-panel-bg"
+                :src="'./assets/' + shops[key].slug + '.jpeg'"
+              />
+              <input type="checkbox" :id="key" v-model="shops[key].isActive" />
+            </label>
+          </div>
         </div>
       </div>
+      <input
+        type="radio"
+        id="member"
+        name="memberStatus"
+        value="member"
+        v-model="memberStatus"
+      />
+      <label for="member">Mitglied</label><br />
+      <input
+        type="radio"
+        id="user"
+        name="memberStatus"
+        value="user"
+        v-model="memberStatus"
+      />
+      <label for="user">Nutzer</label><br />
       <div class="submit">
         <button
           @click="checkout"
